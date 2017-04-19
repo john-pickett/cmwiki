87fbf5c1f44665c2643f4c4143faf9315a792f31
John P <john.pickett@gmail.com>
2017-04-19 15:12:02 -0500

    tracking code 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ var statshound = {};
 statshound.report = function(eventName) {
   var event = {event: { name: eventName }};
   var request = new XMLHttpRequest();
-  request.open("POST", "http://localhost:3000/api/events", true);
+  request.open("POST", "https://stats-hound-97714.herokuapp.com/api/events", true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify(event));
   }
