a7684fb953616dab94f99cb4ce0db99a18c913ca
John P <john.pickett@gmail.com>
2017-05-04 16:26:27 -0500

    removed froala

--- Gemfile
+++ Gemfile
@@ -33,8 +33,7 @@ gem 'devise'
 gem 'faker'
 # Red Carpet for markdown on wikis
 gem 'redcarpet'
-# Froala WSYIWYG Editor
-gem 'wysiwyg-rails'
+
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,18 +15,3 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
-//= require froala_editor.min.js
-
-$('selector').froalaEditor();
-
-var statshound = {};
-
-statshound.report = function(eventName) {
-  var event = {event: { name: eventName }};
-  var request = new XMLHttpRequest();
-  request.open("POST", "https://stats-hound-97714.herokuapp.com/api/events", true);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify(event));
-  }
-
-statshound.report("pageview");
