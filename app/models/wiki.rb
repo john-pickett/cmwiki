fb7d6231c237a07d458c6dc1d9046c54cc122757
John P <john.pickett@gmail.com>
2017-08-21 16:13:22 -0500

    added age range menu to new and edit views

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,6 +2,7 @@ class Wiki < ApplicationRecord
   belongs_to :user
   has_paper_trail
   CATEGORIES = ["Literature", "Science", "Math", "Biography", "History", "Poetry", "Nature", "Bible", "Health", "Home Ec", "Other"]
+  AGE_RANGES = ["6 to 8 years old", "9 to 12 years old", "13 to 15 years old", "16 to 18 years old"]
 
   def self.search(search)
     where("title ILIKE ? OR author ILIKE ? OR category ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
