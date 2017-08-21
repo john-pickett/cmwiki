18c84f0e9b6b0342e7ab8443c0338435a123a59e
John P <john.pickett@gmail.com>
2017-08-21 10:43:20 -0500

    added new book categories

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,7 +1,7 @@
 class Wiki < ApplicationRecord
   belongs_to :user
   has_paper_trail
-  CATEGORIES = ["Literature", "Science", "Math", "Biography", "History"]
+  CATEGORIES = ["Literature", "Science", "Math", "Biography", "History", "Poetry", "Nature", "Bible", "Health", "Home Ec", "Other"]
 
   def self.search(search)
     where("name ILIKE ? OR author ILIKE ? OR category ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :wikis
   devise_for :users
-  root 'welcome#index'
+  root 'wikis#index'
   get '/wikis/:id/history', to: 'wikis#history', as: :wikis_history
 
 end
